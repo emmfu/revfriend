62ab1c393027f26c7c32a9024004bfc480fa58d7
emmfu <efuenteswk@gmail.com>
2022-06-05 21:11:12 -0700

    push

--- src/main/java/com/revature/controllers/TestController.java
+++ src/main/java/com/revature/controllers/TestController.java
@@ -1,5 +1,13 @@
 package com.revature.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
+	@GetMapping("/health")
+	public String health() {
+		return "Welcome friend !";
+	}
 }
